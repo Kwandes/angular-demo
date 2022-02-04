53bf7fb0ed29e8b419242c7aaf98059436aa3aab
Kwandes <bz.jasiek@gmail.com>
2022-02-04 20:01:09 +0100

    feat: 🎸 Fully style the footer, add pending tasks

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  pendingTasks = 3; // TODO - set it based on other task information
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getPendingTasks(): string {
+    if (this.pendingTasks === 1) return '1 pending task';
+    if (this.pendingTasks > 1) return `${this.pendingTasks} pending tasks`;
+    return '';
   }
-
 }
