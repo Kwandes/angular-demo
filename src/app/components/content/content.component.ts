3446254ae54c227e780485495040c22f047cf6be
Kwandes <bz.jasiek@gmail.com>
2022-02-04 20:15:29 +0100

    style: 💄 Add basic layout for the content section

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
+  username = 'Michael Hansen';
+  pendingTasks = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
